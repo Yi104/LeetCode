9866d92ff2e8c2185635adfd61541d32f6103b90
Yi104 <52970186+Yi104@users.noreply.github.com>
2023-12-12 21:13:01 -0800

    Create 6.ZigzagConversion.py

---
+++ 6.ZigzagConversion.py
@@ -0,0 +1,23 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        result = [''] * numRows
+        index, step = 0, 1
+
+        for char in s:
+            result[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+
+        return ''.join(result)
+
+# Example usage:
+solution = Solution()
+print(solution.convert("PAYPALISHIRING", 3))  # Output: "PAHNAPLSIIGYIR"
+print(solution.convert("PAYPALISHIRING", 4))  # Output: "PINALSIGYAHRPI"
+print(solution.convert("A", 1))               # Output: "A"
