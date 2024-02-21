23c8915637faee5963a7690fe15b159fe01ab5f4
Yi104 <52970186+Yi104@users.noreply.github.com>
2024-02-20 22:37:12 -0800

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,35 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        def is_not_under_attack(row, col):
+            return not (cols[col] or hill_diagonals[row - col] or dale_diagonals[row + col])
+
+        def place_queen(row, col):
+            queens.add((row, col))
+            cols[col] = 1
+            hill_diagonals[row - col] = 1
+            dale_diagonals[row + col] = 1
+
+        def remove_queen(row, col):
+            queens.remove((row, col))
+            cols[col] = 0
+            hill_diagonals[row - col] = 0
+            dale_diagonals[row + col] = 0
+
+        def backtrack(row):
+            nonlocal count
+            for col in range(n):
+                if is_not_under_attack(row, col):
+                    place_queen(row, col)
+                    if row + 1 == n:
+                        count += 1
+                    else:
+                        backtrack(row + 1)
+                    remove_queen(row, col)
+
+        cols = [0] * n
+        hill_diagonals = [0] * (2 * n - 1)
+        dale_diagonals = [0] * (2 * n - 1)
+        queens = set()
+        count = 0
+        backtrack(0)
+        return count
