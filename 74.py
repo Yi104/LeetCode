adba6c625a3dcdcd47b530156ad1b33d93d1e95e
Yi104 <52970186+Yi104@users.noreply.github.com>
2024-03-14 22:03:45 -0700

    Create 74.py

---
+++ 74.py
@@ -0,0 +1,18 @@
+def searchMatrix(matrix, target):
+    if not matrix or not matrix[0]:
+        return False
+    
+    rows, cols = len(matrix), len(matrix[0])
+    left, right = 0, rows * cols - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        num = matrix[mid // cols][mid % cols]
+        if num == target:
+            return True
+        elif num < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+            
+    return False
